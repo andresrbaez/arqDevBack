6f664fcfe9b6584ef2c0f3f1286a315e157d3023
Andres Felipe Rodriguez Baez <andres.rodriguezb@anh.gov.co>
2023-09-27 12:48:05 -0500

    cors

--- server/settings.py
+++ server/settings.py
@@ -135,3 +135,8 @@ if not DEBUG:
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# cors auth
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
+
+CSRF_TRUSTED_ORIGINS = ["http://*"]
